3fa2aa9b03bde75b741c0b707000c66fb612421d
bswierzewski <swierzewski.bartosz@gmail.com>
2026-04-12 22:10:30 +0200

    Add migrations

---
+++ src/backend/Modules/Weather/Scaffold.Weather/Infrastructure/Persistence/Migrations/20260412195819_Auto_20260412_215817.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scaffold.Weather.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Auto_20260412_215817 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "weather");
+
+            migrationBuilder.CreateTable(
+                name: "WeatherForecasts",
+                schema: "weather",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    TemperatureC = table.Column<int>(type: "integer", nullable: false),
+                    Summary = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecasts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecasts",
+                schema: "weather");
+        }
+    }
+}
