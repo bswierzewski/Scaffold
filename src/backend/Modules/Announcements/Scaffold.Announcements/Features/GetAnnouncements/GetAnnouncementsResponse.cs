6ecc403a8330ec81809cf1fbbaf6df176eadfe2d
bswierzewski <swierzewski.bartosz@gmail.com>
2026-04-18 22:31:15 +0200

    Remove modules

--- src/backend/Modules/Announcements/Scaffold.Announcements/Features/GetAnnouncements/GetAnnouncementsResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace Scaffold.Announcements.Features.GetAnnouncements;
-
-public sealed record GetAnnouncementsResponse(
-    Guid Id,
-    string Title,
-    string Content,
-    DateTimeOffset PublishedAt);
--- tests/Scaffold.Tests.Integration/Shared/ScaffoldEnvironment.cs
+++ tests/Scaffold.Tests.Integration/Shared/ScaffoldEnvironment.cs
@@ -1,8 +1,6 @@
 using BuildingBlocks.Tests.Integration;
 using BuildingBlocks.Tests.Integration.Utils;
 using Microsoft.Extensions.DependencyInjection;
-using Scaffold.Announcements.Infrastructure.Persistence;
-using Scaffold.Weather.Infrastructure.Persistence;
 
 namespace Scaffold.Tests.Integration.Shared;
 
@@ -12,16 +10,11 @@ namespace Scaffold.Tests.Integration.Shared;
 /// </summary>
 public sealed class ScaffoldEnvironment : IntegrationTestEnvironment<Program>
 {
-    public string A = "Hello";
-
     public override void ConfigureServices(IServiceCollection services)
     {
-        services.AddScoped<BuildingBlocks.Core.Abstractions.ICurrentUser, IntegrationCurrentUserHandler>();
     }
 
     protected override async ValueTask InitializeEnvironmentAsync()
     {
-        await IntegrationTestDatabaseUtils.MigrateDatabaseAsync<AnnouncementsDbContext>(ConnectionString);
-        await IntegrationTestDatabaseUtils.MigrateDatabaseAsync<WeatherDbContext>(ConnectionString);
     }
 }
